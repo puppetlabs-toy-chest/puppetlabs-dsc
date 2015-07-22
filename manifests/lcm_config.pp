bd55c5cc1653704b8001fc6833b7f42c51bb1354
James Pogran <james.pogran@puppetlabs.com>
2015-07-22 15:14:13 -0400

    (MODULES-2243) Remove LCM Refreshmode Change
    Removes setting the LCM RefreshMode in the Invoke DSC Resource script
    and instead bails out if it is set to anything else than Disabled.
    
    Creates a Puppet type that will set the LCM RefreshMode to one of the
    supported values. User must specify this before any DSC Resource
    statments in their manifest. It uses the CIM methods to changes the LCM
    refreshmode instead of PowerShell cmdlets to increase execution speed
    and avoid compiling MOFs to disk

---
+++ manifests/lcm_config.pp
@@ -0,0 +1,12 @@
+define dsc::lcm_config (
+  $refresh_mode = 'Disabled'
+) {
+
+  validate_re($refresh_mode, '^(Disabled|Pull|Push)$', 'refresh_mode must be one of \'Disabled\', \'Push\', \'Pull\'')
+
+  exec { "dsc_provider_set_lcm_refreshmode_${refresh_mode}":
+    provider => 'powershell',
+    onlyif   => "if ((Get-DscLocalConfigurationManager).RefreshMode -eq '${refresh_mode}') {exit 1} else {exit 0}",
+    command  => template('dsc/set-lcm-mode.ps1.erb'),
+  }
+}
