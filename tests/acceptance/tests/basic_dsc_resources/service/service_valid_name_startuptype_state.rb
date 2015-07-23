e54db917ce76e939e5a9b380de8691be0d80715f
Ryan Gard <ryan.gard@puppetlabs.com>
2015-07-23 11:20:27 -0700

    (MODULES-2257) Remove Extra Confines

--- tests/acceptance/tests/basic_dsc_resources/service/service_valid_name_startuptype_state.rb
+++ tests/acceptance/tests/basic_dsc_resources/service/service_valid_name_startuptype_state.rb
@@ -2,8 +2,9 @@ require 'erb'
 require 'dsc_utils'
 test_name 'MODULES-2264 - C68727 - Apply DSC Service Resource with Valid "Name", "StartupType" and "State" Specified'
 
+confine(:to, :platform => 'windows')
+
 # Init
-test_dir_name = 'test'
 local_files_root_path = ENV['MANIFESTS'] || 'tests/manifests'
 
 # ERB Manifest
@@ -19,33 +20,29 @@ dsc_manifest = ERB.new(File.read(dsc_manifest_template_path), 0, '>').result(bin
 
 # Teardown
 teardown do
-  confine_block(:to, :platform => 'windows') do
-    step 'Remove Test Artifacts'
-    set_dsc_resource(
-      agents,
-      dsc_type,
-      :Name        => 'w32time',
-      :State       => 'Stopped',
-      :StartupType => 'Manual'
-    )
-  end
+  step 'Remove Test Artifacts'
+  set_dsc_resource(
+    agents,
+    dsc_type,
+    :Name        => 'w32time',
+    :State       => 'Stopped',
+    :StartupType => 'Manual'
+  )
 end
 
 # Tests
-confine_block(:to, :platform => 'windows') do
-  agents.each do |agent|
-    step 'Apply Manifest'
-    on(agent, puppet('apply'), :stdin => dsc_manifest, :acceptable_exit_codes => [0,2]) do |result|
-      assert_no_match(/Error:/, result.stderr, 'Unexpected error was detected!')
-    end
-
-    step 'Verify Results'
-    assert_dsc_resource(
-      agent,
-      dsc_type,
-      :Name        => dsc_props[:dsc_name],
-      :State       => dsc_props[:dsc_state],
-      :StartupType => dsc_props[:dsc_startuptype]
-    )
+agents.each do |agent|
+  step 'Apply Manifest'
+  on(agent, puppet('apply'), :stdin => dsc_manifest, :acceptable_exit_codes => [0,2]) do |result|
+    assert_no_match(/Error:/, result.stderr, 'Unexpected error was detected!')
   end
+
+  step 'Verify Results'
+  assert_dsc_resource(
+    agent,
+    dsc_type,
+    :Name        => dsc_props[:dsc_name],
+    :State       => dsc_props[:dsc_state],
+    :StartupType => dsc_props[:dsc_startuptype]
+  )
 end
