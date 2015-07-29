210a1a042a9a4f37934f3289ffe14a75789c9349
Ryan Gard <ryan.gard@puppetlabs.com>
2015-07-29 10:36:36 -0700

    (MODULES-2283) Update LCM Config Pre-suite
    The DSC module now provides a Puppet type for configuring the LCM on
    agents. Update the LCM configuration pre-suite to utilize the new LCM type.
    Also, rename the pre-suite file to fix the lame typo.

---
+++ tests/pre-suite/01_configure_lcm.rb
@@ -0,0 +1,16 @@
+test_name 'FM-2626 - C70297 - Configure LCM for "Disabled" Refresh Mode'
+
+confine(:to, :platform => 'windows')
+
+#Init
+dsc_conf_manifest = <<-MANIFEST
+dsc::lcm_config {'disable_lcm':
+  refresh_mode => 'Disabled'
+}
+MANIFEST
+
+#Setup
+step 'Disable LCM on Windows Agents'
+on(agent, puppet('apply'), :stdin => dsc_conf_manifest, :acceptable_exit_codes => [0,2]) do |result|
+  assert_no_match(/Error:/, result.stderr, 'Unexpected error was detected!')
+end
