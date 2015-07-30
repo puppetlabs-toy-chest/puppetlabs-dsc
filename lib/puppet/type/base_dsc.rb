255e6c21640c678c71ccc9900061c09d4238b5f1
Ethan J. Brown <Iristyle@github>
2015-07-29 23:47:10 -0700

    (maint) unused DscTypeHelpers / refactor string
     - Move string monkey patch to a separate file
       that will later be removed.
     - DscTypeHelpers has no usable code, so remove it

--- lib/puppet/type/base_dsc.rb
+++ lib/puppet/type/base_dsc.rb
@@ -1,10 +1,8 @@
 # This is the base type for dsc.
 # Used to inherit the providers for the generated DSC resources
-require 'puppet_x/msutter/helpers/dsc_type_helpers'
+require 'puppet_x/string'
 
 Puppet::Type.newtype(:base_dsc) do
-
-  include PuppetX::Msutter::Helpers::DscTypeHelpers
 
   newparam(:name, :namevar => true ) do
   end
