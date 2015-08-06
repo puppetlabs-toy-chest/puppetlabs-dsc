60f23afdc65761354663276707b5f470b8229bc0
Ryan Gard <ryan.gard@puppetlabs.com>
2015-08-06 10:09:43 -0700

    (MODULES-2365) Allow Tests to be Ran Locally
    The acceptance and integration tests can only be ran against artifacts
    on the module forge. This is problematic for developers that want to test
    their changes before commiting code. The changes made update the test run
    scripts, configs and documentation to allow for local testing.

--- tests/acceptance/pre-suite/02_dsc_module_install.rb
+++ tests/acceptance/pre-suite/02_dsc_module_install.rb
@@ -2,6 +2,19 @@ test_name 'FM-2626 - C68503 - Install Module via PMT on Agent with Prerequisites
 
 confine(:to, :platform => 'windows')
 
-step 'Install Module via PMT'
-stub_forge_on(agents)
-on(agents, puppet('module install puppetlabs-dsc --module_working_dir C:/Windows/Temp'))
+# Init
+proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+staging = { :module_name => 'puppetlabs-dsc' }
+
+
+agents.each do |agent|
+  local = { :module_name => 'dsc', :proj_root => proj_root, :target_module_path => agent['distmoduledir'] }
+
+  step 'Install DSC Module Dependencies'
+  on(agent, puppet('module install puppetlabs-stdlib'))
+  on(agent, puppet('module install puppetlabs-powershell'))
+
+  step 'Install DSC Module'
+  # in CI install from staging forge, otherwise from local
+  install_dev_puppet_module_on(agent, options[:forge_host] ? staging : local)
+end
--- tests/integration/pre-suite/01_dsc_module_install.rb
+++ tests/integration/pre-suite/01_dsc_module_install.rb
@@ -3,3 +3,16 @@ test_name 'FM-2626 - C68506 - Plug-in Sync Module from Master with Prerequisites
 step 'Install Module via PMT'
 stub_forge_on(master)
 on(master, puppet('module install puppetlabs-dsc'))
+
+# Init
+proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+staging = { :module_name => 'puppetlabs-dsc' }
+local = { :module_name => 'dsc', :proj_root => proj_root, :target_module_path => master['distmoduledir'] }
+
+step 'Install DSC Module Dependencies'
+on(master, puppet('module install puppetlabs-stdlib'))
+on(master, puppet('module install puppetlabs-powershell'))
+
+step 'Install DSC Module'
+# in CI install from staging forge, otherwise from local
+install_dev_puppet_module_on(master, options[:forge_host] ? staging : local)
