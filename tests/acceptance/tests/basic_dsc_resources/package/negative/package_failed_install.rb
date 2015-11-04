fc908952ba0011a73da738d13e7724620c7493c0
Ryan Gard <ryan.gard@puppetlabs.com>
2015-11-04 15:13:16 -0800

    (MODULES-2562) Fix Intermitent Failing Package Tests
    Some of the acceptance tests for the "dsc_package" resource were failing
    sometimes. The root cause is believed to be a poor uninstaller for the NSI
    test program package. The affected tests have been updated to brute force
    uninstall the test package. Also, the test installer binary has been moved
    into the repo for convenience.
    [skip ci]

--- tests/acceptance/tests/basic_dsc_resources/package/negative/package_failed_install.rb
+++ tests/acceptance/tests/basic_dsc_resources/package/negative/package_failed_install.rb
@@ -5,9 +5,13 @@ test_name 'MODULES-2559 - C68690 - Attempt to Apply DSC Package Resource with Pa
 confine(:to, :platform => 'windows')
 
 # Init
-local_files_root_path = ENV['MANIFESTS'] || 'tests/manifests'
-download_url = 'http://int-resources.ops.puppetlabs.net/QA_resources/test_program/test_program_installer.exe'
+local_manifests_root_path = ENV['MANIFESTS'] || 'tests/manifests'
+local_files_root_path = ENV['FILES'] || 'tests/files'
+
+local_installer_file_path = File.join(local_files_root_path, 'test_installer', 'test_program_installer.exe')
 install_path = 'C:\test_program'
+uninstall_reg_key_path = 'Software\Microsoft\Windows\CurrentVersion\Uninstall\TestProgram'
+ps_uninstall_command = "Remove-Item -Recurse -Path HKLM:\\#{uninstall_reg_key_path}"
 
 # ERB Manifest
 dsc_type = 'package'
@@ -20,13 +24,15 @@ dsc_props = {
   :dsc_arguments  => "/S /D=#{install_path}"
 }
 
-dsc_manifest_template_path = File.join(local_files_root_path, 'basic_dsc_resources', 'dsc_single_resource.pp.erb')
+dsc_manifest_template_path = File.join(local_manifests_root_path, 'basic_dsc_resources', 'dsc_single_resource.pp.erb')
 dsc_manifest = ERB.new(File.read(dsc_manifest_template_path), 0, '>').result(binding)
 
 # Teardown
 teardown do
   step 'Remove Test Artifacts'
-  on(agents, "cmd /c \"#{install_path}\\uninstall.exe /S\"")
+  # This invocation will be deprecated by the changes in MODULES-2656
+  # The method will be renamed and marked as public.
+  _exec_dsc_script(agents, "if ( #{ps_uninstall_command} ) { exit 0 } else { exit 1 }")
 
   set_dsc_resource(
     agents,
@@ -36,21 +42,25 @@ teardown do
     :Type            => 'File',
     :DestinationPath => dsc_props[:dsc_path]
   )
+
+  set_dsc_resource(
+    agents,
+    'file',
+    dsc_module,
+    :Ensure          => 'Absent',
+    :Type            => 'Directory',
+    :Force           => '$true',
+    :Recurse         => '$true',
+    :DestinationPath => install_path
+  )
 end
 
 # Verify
 error_msg = /Error:.*The return code 4 was not expected/m
 
 # Setup
-step 'Download Test Package'
-set_dsc_resource(
-  agents,
-  'script',
-  dsc_module,
-  :GetScript  => 'return @{}',
-  :TestScript => "Test-Path '#{dsc_props[:dsc_path]}'",
-  :SetScript  => "(New-Object Net.WebClient).DownloadFile('#{download_url}', '#{dsc_props[:dsc_path]}')"
-)
+step 'Copy Installer to Agents'
+scp_to(agents, local_installer_file_path, dsc_props[:dsc_path])
 
 # Tests
 agents.each do |agent|
