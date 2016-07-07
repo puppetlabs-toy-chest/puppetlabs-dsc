ab744315d629a6a8681f3334ecf43da49f59c4e7
Dave Barnum <dave_barnum@wycliffe.org>
2016-07-07 16:34:53 -0400

    Update xWebAdministration to 1.12

--- lib/puppet_x/dsc_resources/xWebAdministration/DSCResources/MSFT_xWebVirtualDirectory/MSFT_xWebVirtualDirectory.psm1
+++ lib/puppet_x/dsc_resources/xWebAdministration/DSCResources/MSFT_xWebVirtualDirectory/MSFT_xWebVirtualDirectory.psm1
@@ -1,29 +1,51 @@
+# Load the Helper Module
+Import-Module -Name "$PSScriptRoot\..\Helper.psm1"
+
+# Localized messages
+data LocalizedData
+{
+    # culture="en-US"
+    ConvertFrom-StringData -StringData @'
+        VerboseGetTargetResource               = Get-TargetResource has been run.
+        VerboseSetTargetPhysicalPath           = Updating physical path for web virtual directory "{0}".
+        VerboseSetTargetCreateVirtualDirectory = Creating new Web Virtual Directory "{0}".
+        VerboseSetTargetRemoveVirtualDirectory = Removing existing Virtual Directory "{0}".
+        VerboseTestTargetFalse                 = Physical path "{0}" for web virtual directory "{1}" does not match desired state.
+        VerboseTestTargetTrue                  = Web virtual directory is in required state.
+        VerboseTestTargetAbsentTrue            = Web virtual directory "{0}" should be absent and is absent.
+'@
+}
+
 function Get-TargetResource
 {
+    <#
+    .SYNOPSIS
+        This will return a hashtable of results 
+    #>
+    
     [CmdletBinding()]
     [OutputType([System.Collections.Hashtable])]
     param
     (
-        [parameter(Mandatory = $true)]
-        [System.String]
-        $Website,
+        [Parameter(Mandatory = $true)]
+        [String] $Website,
 
-        [parameter(Mandatory = $true)]
-        [System.String]
-        $WebApplication,
+        [Parameter(Mandatory = $true)]
+        [AllowEmptyString()]
+        [String] $WebApplication,
 
-        [parameter(Mandatory = $true)]
-        [System.String]
-        $Name,
+        [Parameter(Mandatory = $true)]
+        [String] $Name,
 
-        [parameter(Mandatory = $true)]
-        [System.String]
-        $PhysicalPath
+        [Parameter(Mandatory = $true)]
+        [String] $PhysicalPath
     )
 
-    Test-Dependancies
+    Assert-Module
 
-    $virtualDirectory = Get-WebVirtualDirectoryInternal -Site $Website -Name $Name -Application $WebApplication
+    $virtualDirectory = Get-WebVirtualDirectory -Site $Website `
+                                                -Name $Name `
+                                                -Application $WebApplication
 
     $PhysicalPath = ''
     $Ensure = 'Absent'
@@ -34,12 +56,14 @@ function Get-TargetResource
         $Ensure = 'Present'
     }
 
+     Write-Verbose -Message ($LocalizedData.VerboseGetTargetResource)
+     
     $returnValue = @{
-        Name = $Name
-        Website = $Website
+        Name           = $Name
+        Website        = $Website
         WebApplication = $WebApplication
-        PhysicalPath = $PhysicalPath
-        Ensure = $Ensure
+        PhysicalPath   = $PhysicalPath
+        Ensure         = $Ensure
     }
 
     return $returnValue
@@ -47,187 +71,131 @@ function Get-TargetResource
 
 function Set-TargetResource
 {
+    <#
+    .SYNOPSIS
+        This will set the desired state
+    #>
+    
     [CmdletBinding()]
     param
     (
-        [parameter(Mandatory = $true)]
-        [System.String]
-        $Website,
-
-        [parameter(Mandatory = $true)]
-        [System.String]
-        $WebApplication,
-
-        [parameter(Mandatory = $true)]
-        [System.String]
-        $Name,
-
-        [parameter(Mandatory = $true)]
-        [System.String]
-        $PhysicalPath,
-
         [ValidateSet('Present','Absent')]
-        [System.String]
-        $Ensure = 'Present'
+        [String] $Ensure = 'Present',
+        
+        [Parameter(Mandatory = $true)]
+        [String] $Website,
+
+        [Parameter(Mandatory = $true)]
+        [AllowEmptyString()]
+        [String] $WebApplication,
+
+        [Parameter(Mandatory = $true)]
+        [String] $Name,
+
+        [Parameter(Mandatory = $true)]
+        [String] $PhysicalPath
     )
 
-    Test-Dependancies
+    Assert-Module
 
     if ($Ensure -eq 'Present')
     {
-        $virtualDirectory = Get-WebVirtualDirectoryInternal -Site $Website -Name $Name -Application $WebApplication
+        $virtualDirectory = Get-WebVirtualDirectory -Site $Website `
+                                                    -Name $Name `
+                                                    -Application $WebApplication
         if ($virtualDirectory.count -eq 0)
         {
-            Write-Verbose "Creating new Web Virtual Directory $Name."
-            New-WebVirtualDirectory -Site $Website -Application $WebApplication -Name $Name -PhysicalPath $PhysicalPath
+            Write-Verbose -Message ($LocalizedData.VerboseSetTargetCreateVirtualDirectory -f $Name)
+            New-WebVirtualDirectory -Site $Website `
+                                    -Application $WebApplication `
+                                    -Name $Name `
+                                    -PhysicalPath $PhysicalPath
         }
         else
         {
-            Write-Verbose "Updating physical path for web virtual directory $Name."
-            Set-ItemProperty -Path IIS:Sites\$Website\$WebApplication\$Name -Name physicalPath -Value $PhysicalPath
+            Write-Verbose -Message ($LocalizedData.VerboseSetTargetPhysicalPath -f $Name)
+
+            if ($WebApplication.Length -gt 0)
+            {
+                $ItemPath = "IIS:Sites\$Website\$WebApplication\$Name"
+            }
+            else
+            {
+                $ItemPath = "IIS:Sites\$Website\$Name"
+            }
+
+            Set-ItemProperty -Path $ItemPath `
+                             -Name physicalPath `
+                             -Value $PhysicalPath
         }
     }
 
     if ($Ensure -eq 'Absent')
     {
-        Write-Verbose "Removing existing Virtual Directory $Name."
-        Remove-WebVirtualDirectory -Site $Website -Application $WebApplication -Name $Name
+        Write-Verbose -Message ($LocalizedData.VerboseSetTargetRemoveVirtualDirectory -f $Name)
+        Remove-WebVirtualDirectory -Site $Website `
+                                   -Application $WebApplication `
+                                   -Name $Name
     }
 }
 
 function Test-TargetResource
 {
+    <#
+    .SYNOPSIS
+        This tests the desired state. If the state is not correct it will return $false.
+        If the state is correct it will return $true
+    #>
+    
     [CmdletBinding()]
     [OutputType([System.Boolean])]
     param
     (
-        [parameter(Mandatory = $true)]
-        [System.String]
-        $Website,
-
-        [parameter(Mandatory = $true)]
-        [System.String]
-        $WebApplication,
-
-        [parameter(Mandatory = $true)]
-        [System.String]
-        $Name,
-
-        [parameter(Mandatory = $true)]
-        [System.String]
-        $PhysicalPath,
-
         [ValidateSet('Present','Absent')]
-        [System.String]
-        $Ensure = 'Present'
+        [String] $Ensure = 'Present',
+        
+        [Parameter(Mandatory = $true)]
+        [String] $Website,
+
+        [Parameter(Mandatory = $true)]
+        [AllowEmptyString()]
+        [String] $WebApplication,
+
+        [Parameter(Mandatory = $true)]
+        [String] $Name,
+
+        [Parameter(Mandatory = $true)]
+        [String] $PhysicalPath
     )
 
-    Test-Dependancies
+    Assert-Module
 
-    Write-Verbose 'Checking the virtual directories for the website.'
-    $virtualDirectory = Get-WebVirtualDirectoryInternal -Site $Website -Name $Name -Application $WebApplication
+    $virtualDirectory = Get-WebVirtualDirectory -Site $Website `
+                                                -Name $Name `
+                                                -Application $WebApplication
 
     if ($virtualDirectory.Count -eq 1 -and $Ensure -eq 'Present')
     {
         if ($virtualDirectory.PhysicalPath -eq $PhysicalPath)
         {
-            Write-Verbose 'Web virtual directory is in required state'
+            Write-Verbose -Message ($LocalizedData.VerboseTestTargetTrue)
             return $true
         }
         else
         {
-            Write-Verbose "Physical path $PhysicalPath for web virtual directory $Name does not match desired state."
+            Write-Verbose -Message ($LocalizedData.VerboseTestTargetFalse -f $PhysicalPath, $Name)
             return $false
         }
     }
 
     if ($virtualDirectory.count -eq 0 -and $Ensure -eq 'Absent')
     {
-        Write-Verbose "Web virtual directory $Name should be absent and is absent"
+        Write-Verbose -Message ($LocalizedData.VerboseTestTargetAbsentTrue -f $Name)
         return $true
     }
 
     return $false
-}
-
-function Test-Dependancies
-{
-    Write-Verbose 'Checking whether WebAdministration is there in the machine or not.'
-    # Check if WebAdministration module is present for IIS cmdlets
-    if(!(Get-Module -ListAvailable -Name WebAdministration))
-    {
-        Throw 'Please ensure that WebAdministration module is installed.'
-    }
-}
-
-function Get-WebVirtualDirectoryInternal
-{
-    param
-    (
-        [parameter(Mandatory = $true)]
-        [System.String]
-        $Name,
-
-        [parameter(Mandatory = $true)]
-        [System.String]
-        $Site,
-
-        [parameter(Mandatory = $true)]
-        [System.String]
-        $Application
-    )
-
-    if ((Test-ApplicationExists -Site $Site -Application $Application) -ne $true)
-    {
-        return Get-WebVirtualDirectory -Site $Site -Name $(Get-CompositeName -Name $Name -Application $Application)
-    }
-
-    return Get-WebVirtualDirectory -Site $Site -Application $Application -Name $Name
-}
-
-function Test-ApplicationExists
-{
-    param
-    (
-        [parameter(Mandatory = $true)]
-        [System.String]
-        $Site,
-
-        [parameter(Mandatory = $true)]
-        [System.String]
-        $Application
-    )
-
-    $WebApplication = Get-WebApplication -Site $Site -Name $Application
-
-    if ($WebApplication.Count -eq 1)
-    {
-        return $true
-    }
-
-    Write-Warning "Specified Web Application $Application does not exist."
-
-    return $false
-}
-
-function Get-CompositeName
-{
-    param
-    (
-        [parameter(Mandatory = $true)]
-        [System.String]
-        $Name,
-
-        [parameter(Mandatory = $true)]
-        [System.String]
-        $Application
-    )
-
-    return "$Application/$Name"
 }
 
 Export-ModuleMember -Function *-TargetResource
-
-
-
 
