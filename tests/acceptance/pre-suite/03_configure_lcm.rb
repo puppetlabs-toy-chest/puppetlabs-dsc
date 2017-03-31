91bbac3b6bc2ce5d9f2968c02a8f1582d9438af4
James Pogran <james.pogran@puppetlabs.com>
2017-03-31 13:53:12 -0400

    (maint) Remove LCM pre-suite
    We haven't needed to configure the LCM before we execute the DSC since before 5.0. This removes the pre-suite setup for lcm

--- tests/acceptance/pre-suite/03_configure_lcm.rb
+++
@@ -1,7 +0,0 @@
-require 'dsc_utils'
-test_name 'FM-2626 - C70297 - Configure LCM for "Disabled" Refresh Mode'
-
-confine(:to, :platform => 'windows')
-
-step 'Disable LCM on Windows Agents'
-configure_lcm(agents, refresh_mode = 'Disabled')
