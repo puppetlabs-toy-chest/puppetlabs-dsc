4a65bf8f0ec22c0fb8c3a8a6a4a62cf7f5b5b95a
James Pogran <james.pogran@puppet.com>
2017-07-25 12:33:43 -0400

    (MODULES-5228) Fix PowerShellManager template path
    This moves the init_ps1.ps1 PowerShellManager template to a subdirectory so that
    there are no conflicts with other modules that use `lib/puppet_x/templates` path.

--- lib/puppet_x/puppetlabs/powershell_manager.rb
+++ lib/puppet_x/puppetlabs/powershell_manager.rb
@@ -121,7 +121,7 @@ module PuppetX
 
       def self.init_path
         # a PowerShell -File compatible path to bootstrap the instance
-        path = File.expand_path('../../templates', __FILE__)
+        path = File.expand_path('../../templates/dsc', __FILE__)
         path = File.join(path, 'init_ps.ps1').gsub('/', '\\')
         "\"#{path}\""
       end
