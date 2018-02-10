2d7f0650c898e5069b53c600da88504f57f2564f
James Pogran <james.pogran@puppet.com>
2018-02-09 20:53:01 -0500

    (MODULES-6592) Update xActiveDirectory to 2.17.0.0
    This commit updates xActiveDirectory to the latest gallery version

--- lib/puppet/type/dsc_xadserviceprincipalname.rb
+++ lib/puppet/type/dsc_xadserviceprincipalname.rb
@@ -1,14 +1,14 @@
 require 'pathname'
 
-Puppet::Type.newtype(:dsc_xaddomaintrust) do
+Puppet::Type.newtype(:dsc_xadserviceprincipalname) do
   require Pathname.new(__FILE__).dirname + '../../' + 'puppet/type/base_dsc'
   require Pathname.new(__FILE__).dirname + '../../puppet_x/puppetlabs/dsc_type_helpers'
 
 
   @doc = %q{
-    The DSC xADDomainTrust resource type.
+    The DSC xADServicePrincipalName resource type.
     Automatically generated from
-    'xActiveDirectory/DSCResources/MSFT_xADDomainTrust/MSFT_xADDomainTrust.schema.mof'
+    'xActiveDirectory/DSCResources/MSFT_xADServicePrincipalName/MSFT_xADServicePrincipalName.schema.mof'
 
     To learn more about PowerShell Desired State Configuration, please
     visit https://technet.microsoft.com/en-us/library/dn249912.aspx.
@@ -21,12 +21,11 @@ Puppet::Type.newtype(:dsc_xaddomaintrust) do
   }
 
   validate do
-      fail('dsc_targetdomainname is a required attribute') if self[:dsc_targetdomainname].nil?
-      fail('dsc_sourcedomainname is a required attribute') if self[:dsc_sourcedomainname].nil?
+      fail('dsc_serviceprincipalname is a required attribute') if self[:dsc_serviceprincipalname].nil?
     end
 
-  def dscmeta_resource_friendly_name; 'xADDomainTrust' end
-  def dscmeta_resource_name; 'MSFT_xADDomainTrust' end
+  def dscmeta_resource_friendly_name; 'xADServicePrincipalName' end
+  def dscmeta_resource_name; 'MSFT_xADServicePrincipalName' end
   def dscmeta_module_name; 'xActiveDirectory' end
   def dscmeta_module_version; '2.17.0.0' end
 
@@ -63,7 +62,7 @@ Puppet::Type.newtype(:dsc_xaddomaintrust) do
   newparam(:dsc_ensure) do
     def mof_type; 'string' end
     def mof_is_embedded?; false end
-    desc "Ensure - Should this resource be present or absent Valid values are Present, Absent."
+    desc "Ensure - Specifies if the service principal name should be added or remove. Default value is 'Present'. { *Present* | Absent }. Valid values are Present, Absent."
     validate do |value|
       resource[:ensure] = value.downcase
       unless value.kind_of?(String)
@@ -75,30 +74,14 @@ Puppet::Type.newtype(:dsc_xaddomaintrust) do
     end
   end
 
-  # Name:         TargetDomainAdministratorCredential
-  # Type:         MSFT_Credential
-  # IsMandatory:  False
-  # Values:       None
-  newparam(:dsc_targetdomainadministratorcredential) do
-    def mof_type; 'MSFT_Credential' end
-    def mof_is_embedded?; true end
-    desc "TargetDomainAdministratorCredential - Credentials to authenticate to the target domain"
-    validate do |value|
-      unless value.kind_of?(Hash)
-        fail("Invalid value '#{value}'. Should be a hash")
-      end
-      PuppetX::Dsc::TypeHelpers.validate_MSFT_Credential("TargetDomainAdministratorCredential", value)
-    end
-  end
-
-  # Name:         TargetDomainName
+  # Name:         ServicePrincipalName
   # Type:         string
   # IsMandatory:  True
   # Values:       None
-  newparam(:dsc_targetdomainname) do
+  newparam(:dsc_serviceprincipalname) do
     def mof_type; 'string' end
     def mof_is_embedded?; false end
-    desc "TargetDomainName - Name of the AD domain that is being trusted"
+    desc "ServicePrincipalName - The full SPN to add or remove, e.g. HOST/LON-DC1."
     isrequired
     validate do |value|
       unless value.kind_of?(String)
@@ -107,51 +90,14 @@ Puppet::Type.newtype(:dsc_xaddomaintrust) do
     end
   end
 
-  # Name:         TrustType
+  # Name:         Account
   # Type:         string
   # IsMandatory:  False
-  # Values:       ["External", "Forest"]
-  newparam(:dsc_trusttype) do
-    def mof_type; 'string' end
-    def mof_is_embedded?; false end
-    desc "TrustType - Type of trust Valid values are External, Forest."
-    validate do |value|
-      unless value.kind_of?(String)
-        fail("Invalid value '#{value}'. Should be a string")
-      end
-      unless ['External', 'external', 'Forest', 'forest'].include?(value)
-        fail("Invalid value '#{value}'. Valid values are External, Forest")
-      end
-    end
-  end
-
-  # Name:         TrustDirection
-  # Type:         string
-  # IsMandatory:  False
-  # Values:       ["Bidirectional", "Inbound", "Outbound"]
-  newparam(:dsc_trustdirection) do
-    def mof_type; 'string' end
-    def mof_is_embedded?; false end
-    desc "TrustDirection - Direction of trust Valid values are Bidirectional, Inbound, Outbound."
-    validate do |value|
-      unless value.kind_of?(String)
-        fail("Invalid value '#{value}'. Should be a string")
-      end
-      unless ['Bidirectional', 'bidirectional', 'Inbound', 'inbound', 'Outbound', 'outbound'].include?(value)
-        fail("Invalid value '#{value}'. Valid values are Bidirectional, Inbound, Outbound")
-      end
-    end
-  end
-
-  # Name:         SourceDomainName
-  # Type:         string
-  # IsMandatory:  True
   # Values:       None
-  newparam(:dsc_sourcedomainname) do
+  newparam(:dsc_account) do
     def mof_type; 'string' end
     def mof_is_embedded?; false end
-    desc "SourceDomainName - Name of the AD domain that is requesting the trust"
-    isrequired
+    desc "Account - The user or computer account to add or remove the SPN, e.b. User1 or LON-DC1$. Default value is ''. If Ensure is set to Present, a value must be specified."
     validate do |value|
       unless value.kind_of?(String)
         fail("Invalid value '#{value}'. Should be a string")
@@ -166,7 +112,7 @@ Puppet::Type.newtype(:dsc_xaddomaintrust) do
   end
 end
 
-Puppet::Type.type(:dsc_xaddomaintrust).provide :powershell, :parent => Puppet::Type.type(:base_dsc).provider(:powershell) do
+Puppet::Type.type(:dsc_xadserviceprincipalname).provide :powershell, :parent => Puppet::Type.type(:base_dsc).provider(:powershell) do
   confine :true => (Gem::Version.new(Facter.value(:powershell_version)) >= Gem::Version.new('5.0.10586.117'))
   defaultfor :operatingsystem => :windows
 
