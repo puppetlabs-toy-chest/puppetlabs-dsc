6a33cc3c2686ab4fa492aea94b914afc6b0b33a8
James Pogran <james.pogran@puppet.com>
2018-05-02 11:09:29 -0400

    (MODULES-7057) Update xComputerManagement to 4.1.0.0-PSGallery
    This commit updates xComputerManagement to the latest gallery version 4.1.0.0-PSGallery

--- lib/puppet/type/dsc_xofflinedomainjoin.rb
+++ lib/puppet/type/dsc_xofflinedomainjoin.rb
@@ -27,7 +27,7 @@ Puppet::Type.newtype(:dsc_xofflinedomainjoin) do
   def dscmeta_resource_friendly_name; 'xOfflineDomainJoin' end
   def dscmeta_resource_name; 'MSFT_xOfflineDomainJoin' end
   def dscmeta_module_name; 'xComputerManagement' end
-  def dscmeta_module_version; '4.0.0.0' end
+  def dscmeta_module_version; '4.1.0.0' end
 
   newparam(:name, :namevar => true ) do
   end
