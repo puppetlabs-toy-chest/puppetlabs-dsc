182b009a67a7adb284c9d527fd8152615a945380
James Pogran <james.pogran@puppet.com>
2018-05-02 11:08:46 -0400

    (MODULES-7057) Update xActiveDirectory to 2.18.0.0-PSGallery
    This commit updates xActiveDirectory to the latest gallery version 2.18.0.0-PSGallery

--- lib/puppet/type/dsc_xadreplicationsite.rb
+++ lib/puppet/type/dsc_xadreplicationsite.rb
@@ -1,14 +1,14 @@
 require 'pathname'
 
-Puppet::Type.newtype(:dsc_xadrecyclebin) do
+Puppet::Type.newtype(:dsc_xadreplicationsite) do
   require Pathname.new(__FILE__).dirname + '../../' + 'puppet/type/base_dsc'
   require Pathname.new(__FILE__).dirname + '../../puppet_x/puppetlabs/dsc_type_helpers'
 
 
   @doc = %q{
-    The DSC xADRecycleBin resource type.
+    The DSC xADReplicationSite resource type.
     Automatically generated from
-    'xActiveDirectory/DSCResources/MSFT_xADRecycleBin/MSFT_xADRecycleBin.schema.mof'
+    'xActiveDirectory/DSCResources/MSFT_xADReplicationSite/MSFT_xADReplicationSite.schema.mof'
 
     To learn more about PowerShell Desired State Configuration, please
     visit https://technet.microsoft.com/en-us/library/dn249912.aspx.
@@ -21,11 +21,11 @@ Puppet::Type.newtype(:dsc_xadrecyclebin) do
   }
 
   validate do
-      fail('dsc_forestfqdn is a required attribute') if self[:dsc_forestfqdn].nil?
+      fail('dsc_name is a required attribute') if self[:dsc_name].nil?
     end
 
-  def dscmeta_resource_friendly_name; 'xADRecycleBin' end
-  def dscmeta_resource_name; 'MSFT_xADRecycleBin' end
+  def dscmeta_resource_friendly_name; 'xADReplicationSite' end
+  def dscmeta_resource_name; 'MSFT_xADReplicationSite' end
   def dscmeta_module_name; 'xActiveDirectory' end
   def dscmeta_module_version; '2.18.0.0' end
 
@@ -35,6 +35,7 @@ Puppet::Type.newtype(:dsc_xadrecyclebin) do
   ensurable do
     newvalue(:exists?) { provider.exists? }
     newvalue(:present) { provider.create }
+    newvalue(:absent)  { provider.destroy }
     defaultto { :present }
   end
 
@@ -54,14 +55,33 @@ Puppet::Type.newtype(:dsc_xadrecyclebin) do
     end
   end
 
-  # Name:         ForestFQDN
+  # Name:         Ensure
+  # Type:         string
+  # IsMandatory:  False
+  # Values:       ["Present", "Absent"]
+  newparam(:dsc_ensure) do
+    def mof_type; 'string' end
+    def mof_is_embedded?; false end
+    desc "Ensure - Specifies if the AD replication site should be added or remove. Default value is 'Present'. { *Present* | Absent }. Valid values are Present, Absent."
+    validate do |value|
+      resource[:ensure] = value.downcase
+      unless value.kind_of?(String)
+        fail("Invalid value '#{value}'. Should be a string")
+      end
+      unless ['Present', 'present', 'Absent', 'absent'].include?(value)
+        fail("Invalid value '#{value}'. Valid values are Present, Absent")
+      end
+    end
+  end
+
+  # Name:         Name
   # Type:         string
   # IsMandatory:  True
   # Values:       None
-  newparam(:dsc_forestfqdn) do
+  newparam(:dsc_name) do
     def mof_type; 'string' end
     def mof_is_embedded?; false end
-    desc "ForestFQDN"
+    desc "Name - Specifies the name of the AD replication site."
     isrequired
     validate do |value|
       unless value.kind_of?(String)
@@ -70,49 +90,19 @@ Puppet::Type.newtype(:dsc_xadrecyclebin) do
     end
   end
 
-  # Name:         EnterpriseAdministratorCredential
-  # Type:         MSFT_Credential
+  # Name:         RenameDefaultFirstSiteName
+  # Type:         boolean
   # IsMandatory:  False
   # Values:       None
-  newparam(:dsc_enterpriseadministratorcredential) do
-    def mof_type; 'MSFT_Credential' end
-    def mof_is_embedded?; true end
-    desc "EnterpriseAdministratorCredential"
-    validate do |value|
-      unless value.kind_of?(Hash)
-        fail("Invalid value '#{value}'. Should be a hash")
-      end
-      PuppetX::Dsc::TypeHelpers.validate_MSFT_Credential("EnterpriseAdministratorCredential", value)
-    end
-  end
-
-  # Name:         RecycleBinEnabled
-  # Type:         string
-  # IsMandatory:  False
-  # Values:       None
-  newparam(:dsc_recyclebinenabled) do
-    def mof_type; 'string' end
+  newparam(:dsc_renamedefaultfirstsitename) do
+    def mof_type; 'boolean' end
     def mof_is_embedded?; false end
-    desc "RecycleBinEnabled"
+    desc "RenameDefaultFirstSiteName - Specify if the Default-First-Site-Name should be renamed, if it exists. Dafult value is 'false'."
     validate do |value|
-      unless value.kind_of?(String)
-        fail("Invalid value '#{value}'. Should be a string")
-      end
     end
-  end
-
-  # Name:         ForestMode
-  # Type:         string
-  # IsMandatory:  False
-  # Values:       None
-  newparam(:dsc_forestmode) do
-    def mof_type; 'string' end
-    def mof_is_embedded?; false end
-    desc "ForestMode"
-    validate do |value|
-      unless value.kind_of?(String)
-        fail("Invalid value '#{value}'. Should be a string")
-      end
+    newvalues(true, false)
+    munge do |value|
+      PuppetX::Dsc::TypeHelpers.munge_boolean(value.to_s)
     end
   end
 
@@ -123,7 +113,7 @@ Puppet::Type.newtype(:dsc_xadrecyclebin) do
   end
 end
 
-Puppet::Type.type(:dsc_xadrecyclebin).provide :powershell, :parent => Puppet::Type.type(:base_dsc).provider(:powershell) do
+Puppet::Type.type(:dsc_xadreplicationsite).provide :powershell, :parent => Puppet::Type.type(:base_dsc).provider(:powershell) do
   confine :true => (Gem::Version.new(Facter.value(:powershell_version)) >= Gem::Version.new('5.0.10586.117'))
   defaultfor :operatingsystem => :windows
 
--- lib/puppet/type/dsc_xadreplicationsubnet.rb
+++ lib/puppet/type/dsc_xadreplicationsubnet.rb
@@ -1,14 +1,14 @@
 require 'pathname'
 
-Puppet::Type.newtype(:dsc_xadrecyclebin) do
+Puppet::Type.newtype(:dsc_xadreplicationsubnet) do
   require Pathname.new(__FILE__).dirname + '../../' + 'puppet/type/base_dsc'
   require Pathname.new(__FILE__).dirname + '../../puppet_x/puppetlabs/dsc_type_helpers'
 
 
   @doc = %q{
-    The DSC xADRecycleBin resource type.
+    The DSC xADReplicationSubnet resource type.
     Automatically generated from
-    'xActiveDirectory/DSCResources/MSFT_xADRecycleBin/MSFT_xADRecycleBin.schema.mof'
+    'xActiveDirectory/DSCResources/MSFT_xADReplicationSubnet/MSFT_xADReplicationSubnet.schema.mof'
 
     To learn more about PowerShell Desired State Configuration, please
     visit https://technet.microsoft.com/en-us/library/dn249912.aspx.
@@ -21,11 +21,11 @@ Puppet::Type.newtype(:dsc_xadrecyclebin) do
   }
 
   validate do
-      fail('dsc_forestfqdn is a required attribute') if self[:dsc_forestfqdn].nil?
+      fail('dsc_name is a required attribute') if self[:dsc_name].nil?
     end
 
-  def dscmeta_resource_friendly_name; 'xADRecycleBin' end
-  def dscmeta_resource_name; 'MSFT_xADRecycleBin' end
+  def dscmeta_resource_friendly_name; 'xADReplicationSubnet' end
+  def dscmeta_resource_name; 'MSFT_xADReplicationSubnet' end
   def dscmeta_module_name; 'xActiveDirectory' end
   def dscmeta_module_version; '2.18.0.0' end
 
@@ -35,6 +35,7 @@ Puppet::Type.newtype(:dsc_xadrecyclebin) do
   ensurable do
     newvalue(:exists?) { provider.exists? }
     newvalue(:present) { provider.create }
+    newvalue(:absent)  { provider.destroy }
     defaultto { :present }
   end
 
@@ -54,14 +55,33 @@ Puppet::Type.newtype(:dsc_xadrecyclebin) do
     end
   end
 
-  # Name:         ForestFQDN
+  # Name:         Ensure
+  # Type:         string
+  # IsMandatory:  False
+  # Values:       ["Present", "Absent"]
+  newparam(:dsc_ensure) do
+    def mof_type; 'string' end
+    def mof_is_embedded?; false end
+    desc "Ensure - Specifies if the AD replication subnet should be added or remove. Default value is 'Present'. { *Present* | Absent }. Valid values are Present, Absent."
+    validate do |value|
+      resource[:ensure] = value.downcase
+      unless value.kind_of?(String)
+        fail("Invalid value '#{value}'. Should be a string")
+      end
+      unless ['Present', 'present', 'Absent', 'absent'].include?(value)
+        fail("Invalid value '#{value}'. Valid values are Present, Absent")
+      end
+    end
+  end
+
+  # Name:         Name
   # Type:         string
   # IsMandatory:  True
   # Values:       None
-  newparam(:dsc_forestfqdn) do
+  newparam(:dsc_name) do
     def mof_type; 'string' end
     def mof_is_embedded?; false end
-    desc "ForestFQDN"
+    desc "Name - The name of the AD replication subnet, e.g. 10.0.0.0/24."
     isrequired
     validate do |value|
       unless value.kind_of?(String)
@@ -70,30 +90,14 @@ Puppet::Type.newtype(:dsc_xadrecyclebin) do
     end
   end
 
-  # Name:         EnterpriseAdministratorCredential
-  # Type:         MSFT_Credential
-  # IsMandatory:  False
-  # Values:       None
-  newparam(:dsc_enterpriseadministratorcredential) do
-    def mof_type; 'MSFT_Credential' end
-    def mof_is_embedded?; true end
-    desc "EnterpriseAdministratorCredential"
-    validate do |value|
-      unless value.kind_of?(Hash)
-        fail("Invalid value '#{value}'. Should be a hash")
-      end
-      PuppetX::Dsc::TypeHelpers.validate_MSFT_Credential("EnterpriseAdministratorCredential", value)
-    end
-  end
-
-  # Name:         RecycleBinEnabled
+  # Name:         Site
   # Type:         string
   # IsMandatory:  False
   # Values:       None
-  newparam(:dsc_recyclebinenabled) do
+  newparam(:dsc_site) do
     def mof_type; 'string' end
     def mof_is_embedded?; false end
-    desc "RecycleBinEnabled"
+    desc "Site - The name of the assigned AD replication site, e.g. Default-First-Site-Name."
     validate do |value|
       unless value.kind_of?(String)
         fail("Invalid value '#{value}'. Should be a string")
@@ -101,14 +105,14 @@ Puppet::Type.newtype(:dsc_xadrecyclebin) do
     end
   end
 
-  # Name:         ForestMode
+  # Name:         Location
   # Type:         string
   # IsMandatory:  False
   # Values:       None
-  newparam(:dsc_forestmode) do
+  newparam(:dsc_location) do
     def mof_type; 'string' end
     def mof_is_embedded?; false end
-    desc "ForestMode"
+    desc "Location - The location for the AD replication site. Default value is empty."
     validate do |value|
       unless value.kind_of?(String)
         fail("Invalid value '#{value}'. Should be a string")
@@ -123,7 +127,7 @@ Puppet::Type.newtype(:dsc_xadrecyclebin) do
   end
 end
 
-Puppet::Type.type(:dsc_xadrecyclebin).provide :powershell, :parent => Puppet::Type.type(:base_dsc).provider(:powershell) do
+Puppet::Type.type(:dsc_xadreplicationsubnet).provide :powershell, :parent => Puppet::Type.type(:base_dsc).provider(:powershell) do
   confine :true => (Gem::Version.new(Facter.value(:powershell_version)) >= Gem::Version.new('5.0.10586.117'))
   defaultfor :operatingsystem => :windows
 
