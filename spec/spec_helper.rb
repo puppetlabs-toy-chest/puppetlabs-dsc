84524d934633739780f7741b5a480295121f9f39
Ciaran McCrisken <ciaran.mccrisken@puppet.com>
2019-10-09 15:05:02 +0100

    (MODULES-9594) Fix incorrect .sync.yaml additions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 RSpec.configure do |c|
-  c.mock_with rspec
+  c.mock_with :rspec
 end
 
 require 'puppetlabs_spec_helper/module_spec_helper'
@@ -43,6 +43,7 @@ RSpec.configure do |c|
   end
   c.filter_run_excluding(bolt: true) unless ENV['GEM_BOLT']
   c.after(:suite) do
+    RSpec::Puppet::Coverage.report!(0)
   end
 end
 
